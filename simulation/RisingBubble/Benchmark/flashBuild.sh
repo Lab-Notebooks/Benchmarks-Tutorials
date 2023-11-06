08357290ff092cd875ad2a48a6373840ad5a9298
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-11-05 22:24:19 -0600

    Redesign toml files and add outflow test

--- simulation/RisingBubble/Benchmark/flashBuild.sh
+++ simulation/RisingBubble/Benchmark/flashBuild.sh
@@ -2,13 +2,10 @@
 NodeDir=$(realpath .)
 
 # run Flash-X setup
-cd $FLASHX_HOME && ./setup $FlashOptions
+cd $FLASHX_HOME && echo Flash-X HEAD is at $(git rev-parse --short HEAD) && ./setup $FlashOptions
 
 # compile the simulation and copy files
 cd $FLASHX_HOME/object && make -j && cp flashx $NodeDir/
 
 # copy AMReX configuration
 cd $AMREX2D_HOME/lib/pkgconfig && cp amrex.pc $NodeDir/
-
-# chdir into node directory and do clean up
-cd $NodeDir && rm -rf $FLASHX_HOME/object
---
+++ simulation/RisingBubble/OutflowTest/flashOptions.sh
@@ -0,0 +1,4 @@
+# cache the value of current working directory
+
+FlashOptions="incompFlow/RisingBubble -auto -maxblocks=100 +amrex +parallelIO SimForceInOut=True \
+	      InsPresLaplacian=False -site=$SiteHome IOWriteGridFiles=True -2d -nxb=8 -nyb=8"
