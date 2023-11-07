0e388e49b11be1b81ccc5307989822b007063d07
akashdhruv <adhruv@login2.summit.olcf.ornl.gov>
2023-11-07 18:33:31 -0500

    Make changes for summit

--- simulation/RisingBubble/Benchmark/flashOptions.sh
+++ simulation/RisingBubble/Benchmark/flashOptions.sh
@@ -1,4 +1,6 @@
 # cache the value of current working directory
 
+FlashSha="1aff5889"
+
 FlashOptions="incompFlow/RisingBubble -auto -maxblocks=100 +amrex +parallelIO \
 	      InsPresLaplacian=False -site=$SiteHome IOWriteGridFiles=True -2d -nxb=8 -nyb=8"
--- simulation/RisingBubble/OutflowTest/flashBuild.sh
+++ simulation/RisingBubble/OutflowTest/flashBuild.sh
@@ -2,7 +2,8 @@
 NodeDir=$(realpath .)
 
 # run Flash-X setup
-cd $FLASHX_HOME && echo Flash-X HEAD is at $(git rev-parse --short HEAD) && ./setup $FlashOptions
+cd $FLASHX_HOME && echo Flash-X HEAD is at $(git rev-parse --short HEAD)
+cd $FLASHX_HOME && git checkout $FlashSha && ./setup $FlashOptions
 
 # compile the simulation and copy files
 cd $FLASHX_HOME/object && make -j && cp flashx $NodeDir/ && cp setup_params $NodeDir/
