727d73a56932828c3b9fe659ab229eeee987388a
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-11-08 00:29:47 -0600

    revert to serial IO for temporary fix

--- simulation/RisingBubble/Benchmark/flashOptions.sh
+++ simulation/RisingBubble/Benchmark/flashOptions.sh
@@ -2,5 +2,5 @@
 
 FlashSha="1aff5889"
 
-FlashOptions="incompFlow/RisingBubble -auto -maxblocks=100 +amrex +parallelIO \
+FlashOptions="incompFlow/RisingBubble -auto -maxblocks=100 +amrex +serialIO \
 	      InsPresLaplacian=False -site=$SiteHome IOWriteGridFiles=True -2d -nxb=8 -nyb=8"
--- simulation/RisingBubble/OutflowTest/flashOptions.sh
+++ simulation/RisingBubble/OutflowTest/flashOptions.sh
@@ -2,5 +2,5 @@
 
 FlashSha="1aff5889"
 
-FlashOptions="incompFlow/RisingBubble -auto -maxblocks=10 +amrex +parallelIO SimForceInOut=True \
+FlashOptions="incompFlow/RisingBubble -auto -maxblocks=10 +amrex +serialIO SimForceInOut=True \
 	      InsPresLaplacian=False -site=$SiteHome IOWriteGridFiles=True -2d -nxb=8 -nyb=8"
