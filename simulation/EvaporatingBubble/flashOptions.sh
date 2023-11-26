5c90f401d46c9a6a13e3cf9d700364e4cfb2c124
akashdhruv <akashdhruv@gwmail.gwu.edu>
2023-11-25 19:01:20 -0600

    Add an evaporating bubble sim

---
+++ simulation/EvaporatingBubble/flashOptions.sh
@@ -0,0 +1,6 @@
+# Set Flash-X options and SHA
+
+FlashSha="1aff5889"
+
+FlashOptions="incompFlow/EvaporatingBubble -auto -maxblocks=10 +amrex +serialIO SimForceInOut=True \
+	      InsPresLaplacian=False -site=$SiteHome IOWriteGridFiles=True -2d -nxb=16 -nyb=16"
