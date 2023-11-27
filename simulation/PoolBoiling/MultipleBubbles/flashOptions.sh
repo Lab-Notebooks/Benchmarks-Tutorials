8216defc8476d16def329785ddaff73c4fa85510
akashdhruv <adhruv@login5.summit.olcf.ornl.gov>
2023-11-27 14:57:17 -0500

    Add new case for multiple bubbles

---
+++ simulation/PoolBoiling/MultipleBubbles/flashOptions.sh
@@ -0,0 +1,6 @@
+# Set Flash-X options and SHA
+
+FlashSha="1aff5889"
+
+FlashOptions="incompFlow/PoolBoiling -auto -maxblocks=15 +amrex +serialIO SimForceInOut=True \
+	      InsPresLaplacian=False -site=$SiteHome IOWriteGridFiles=True -2d -nxb=16 -nyb=16"
