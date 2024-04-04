f8f8b4e040bd9c59a29b839ffae4c0e2901b7b66
akashdhruv <akashdhruv@gwmail.gwu.edu>
2024-04-03 19:20:17 -0500

    save changes to module

--- analysis/PoolBoiling/LocalModule.py
+++ analysis/PoolBoiling/LocalModule.py
@@ -5,6 +5,7 @@ import itertools
 import numpy
 import boxkit
 import boxkit.resources.flash as flash_box
+from boxkit.library import Action
 
 SIM_LENGTH_SCALE = 1e-3
 SIM_TIME_SCALE = 10e-3
@@ -132,7 +133,6 @@ def lb_noadv_contour_dict():
 
     return dataset_dir, file_tags
 
-
 def lb_noadv_comparison_dict():
     """
     Comparison dict for different outflow buffers
@@ -171,7 +171,120 @@ def lb_noadv_comparison2_dict():
 
     return dataset_dir, file_tags
 
+def temp_stats_dict():
+    """
+    Dictionary for temporal statistics
+    """
+    dataset_dir = {}
+    dataset_dir["reference"] = f"{SIM_PATH}/SingleBubble/outflow/refLong-24/jobnode.archive/2023-11-28"
+    dataset_dir["lb-0.5"] = f"{SIM_PATH}/SingleBubble/outflow/lb_0.5/jobnode.archive/2023-11-26-propSmear"
+    dataset_dir["lb-1.0"] = f"{SIM_PATH}/SingleBubble/outflow/lb_1.0/jobnode.archive/2023-11-26-propSmear"
+    dataset_dir["lb-1.5"] = f"{SIM_PATH}/SingleBubble/outflow/lb_1.5/jobnode.archive/2023-11-26-propSmear"
 
+    file_tags = {}
+    file_tags["reference"] = [*range(5,390,5)]
+    file_tags["lb-0.5"] = [*range(5,440,5)]
+    file_tags["lb-1.0"] = [*range(5,440,5)]
+    file_tags["lb-1.5"] = [*range(5,440,5)]
+
+    return dataset_dir, file_tags
+
+def temporal_stats(datasets, backend="serial", nthreads=1):
+    """
+    Compute variance and mean across a dataset list
+    """
+    # TODO: Add more error handling here to account # pylint: disable=fixme
+    # for consistency between multiple datasets
+    #
+    # Handle errors, compute level of the first
+    # block and raise error if not same for the rest
+    level = datasets[0].blocklist[0].level
+    for dataset in datasets:
+        for block in dataset.blocklist:
+            if block.level != level:
+                raise ValueError(
+                    f"[LocalModule.mean_temporal] All blocks must be at level {level}"
+                )
+
+    varlist_dataset = ["vely", "temp", "dfun"]
+    varlist_stats = [
+        "vely_mean",
+        "temp_mean",
+        "dfun_mean",
+        "turb_yflux",
+        "vely_fluc",
+        "temp_fluc",
+        "dfun_fluc",
+    ]
+
+    # Create an mean dataset
+    stats_dataset = datasets[0].clone(storage="numpy-memmap")
+
+    # loop over varlist append values to
+    # add it to the variance dataset and peform calculations
+    for varkey in varlist_stats:
+        stats_dataset.addvar(varkey)
+
+    sample_size = len(datasets)
+
+    # Create a block list for reduction, first add
+    # blocks from average_dataset and then loop over
+    # datasets to add blocks from their respective blocklist
+    blk_reduce_list = [[block] for block in stats_dataset.blocklist]
+
+    for dataset in datasets:
+        for block, blk_list in zip(dataset.blocklist, blk_reduce_list):
+            blk_list.append(block)
+
+    for varkey in varlist_dataset:
+        Action(mean_blk_list, nthreads=nthreads, backend=backend)(
+            (blk_list for blk_list in blk_reduce_list),
+            varkey,
+            sample_size,
+        )
+
+        Action(variance_blk_list, nthreads=nthreads, backend=backend)(
+            (blk_list for blk_list in blk_reduce_list),
+            varkey,
+            sample_size,
+        )
+
+    Action(turb_blk_list, nthreads=nthreads, backend=backend)(
+        (blk_list for blk_list in blk_reduce_list),
+        sample_size,
+    )
+
+    return boxkit.mergeblocks(stats_dataset, stats_dataset.varlist)
+
+
+def variance_blk_list(blk_list, varkey, sample_size):
+    """
+    Reduce dataset / compute average
+    """
+    stats_blk = blk_list[0]
+
+    for work_blk in blk_list[2:]:
+        stats_blk[varkey+"_fluc"] = (stats_blk[varkey+"_fluc"] 
+                                  + (work_blk[varkey] - stats_blk[varkey+"_mean"])**2/sample_size)
+
+def turb_blk_list(blk_list, sample_size):
+    """
+    Reduce dataset / compute average
+    """
+    stats_blk = blk_list[0]
+
+    for work_blk in blk_list[2:]:
+        stats_blk["turb_yflux"] = (stats_blk["turb_yflux"]
+            + (work_blk["temp"]-stats_blk["temp_mean"])*(work_blk["vely"]-stats_blk["vely_mean"])/sample_size)
+
+def mean_blk_list(blk_list, varkey, sample_size):
+    """
+    Reduce dataset / compute average
+    """
+    stats_blk = blk_list[0]
+
+    for work_blk in blk_list[1:]:
+        stats_blk[varkey+"_mean"] = stats_blk[varkey+"_mean"] + work_blk[varkey]/sample_size
 
 if __name__ == "__main__":
     """
